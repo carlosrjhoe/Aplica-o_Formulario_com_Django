457bee44381d59518f3deed02523a7359d33b681
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-13 15:21:30 -0300

    Criando modelos e melhorando aplicação com MoldelFrorm

--- passagens/forms.py
+++ passagens/forms.py
@@ -5,23 +5,19 @@ from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classes
 from passagens.validation import *
+from passagens.models import Passagem, ClasseViagem, pessoa
 
-class PassagensForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
+class PassagensForms(forms.ModelForm):
     data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
-    classe_viagem = forms.ChoiceField(label='Classe do vôs', choices=tipos_de_classes)
-    informacoes = forms.CharField(
-        label='Informações extras',
-        max_length=200,
-        widget=forms.Textarea(),
-        required=False
-    )
-    email = forms.EmailField(label='Email', max_length=150)
-
-        
+    class Meta:
+        model = Passagem
+        fields = '__all__'
+        labels = {'data_ida':'Data de ida','data_volta':'Data de volta','informacoes':'Informações','classe_viagem':'Classe de vôo'}
+        widgets = {
+            'data_ida':DatePicker(),
+            'data_volta':DatePicker(),
+        }
+  
     def clean(self):
         origem = self.cleaned_data.get('origem',)
         destino = self.cleaned_data.get('destino',)
